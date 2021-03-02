a90d74343c9a7ab2a103745ddf49f4ef8644bd1e
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2021-03-01 22:38:35 -0600

    Image and video input + video controls

--- ProjectPI/main.cpp
+++ ProjectPI/main.cpp
@@ -1,7 +1,10 @@
 #define no_init_all deprecated
 
 #include <Windows.h>
+#include <Commctrl.h>
+#include <string.h>
 #include <thread>
+#include <cmath>
 #include "DeviceEnumerator.h"
 #include "opencv2/opencv.hpp"
 #include "resource.h"
@@ -9,28 +12,129 @@
 HINSTANCE hInst;
 int appShow;
 HWND mainWindow;
+HWND camSelectorWindow;
 
 HWND hPicDisplay;
 
+
+//Video Controls
+HWND hPlayBtn;
+HWND hProgressSlider;
+HWND hTimeText;
+
+HWND hSourcesCB;
+
 cv::VideoCapture videoCapture;
+cv::Mat currFrame;
+int cvFPS = 0;
+float videoLength = 0;
+std::string videoLengthString = "";
+
+bool pauseVideoCapture = true;
+bool stopVideoCapture = false;
 
 //Window procedure declaration
 LRESULT CALLBACK MainProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK CamSelectProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
+
+void EnableVideoControls(bool enable, int frameCount = 0);
+
+bool GetFile(HWND hwndOwner, int type, char* buff)
+{
+	LPCSTR filtro = 0;
+	switch (type)
+	{
+	case 0:
+		filtro = "Imagenes (.jpg,.bmp)\0*.jpg;*.bmp\0Imagenes JPG (.jpg)\0*.jpg\0Imagenes BMP (.bmp)\0*.bmp\0";
+		break;
+	case 1:
+		filtro = "Todos\0*.*\0Videos Mp4\0*.mp4\0";
+		break;
+	case 2:
+		filtro = "Todos\0*.*\0Videos Avi\0*.avi\0";
+	default:
+		break;
+	}
+
+	char szFile[MAX_PATH];  // buffer for file name
+	OPENFILENAME ofn;       // common dialog box structure
+	HANDLE hf;              // file handle
+
+							// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwndOwner;
+	ofn.lpstrFile = szFile;
+	// Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+	// use the contents of szFile to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrFilter = filtro;
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	if (type == 0) {
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	}
+
+	// Display the Open dialog box.
+	if (GetOpenFileName(&ofn) == TRUE) {
+		strcpy_s(buff, MAX_PATH, ofn.lpstrFile);
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+std::string SecToMinAndSecString(float sec) {
+	int min = sec / 60;
+	int lSec = (int)sec % 60;
+	std::string ss = (lSec < 10 ? "0": "") + std::to_string(lSec);
+	return std::to_string(min) + ":" + ss;
+}
 
 
-bool stopVideoCapture = false;
+void replaceCurrentFrame(cv::Mat frame) {
+	cv::cvtColor(frame, frame, cv::COLOR_BGR2BGRA);
+
+	HBITMAP hB = CreateBitmap(frame.cols, frame.rows, 1, 32, frame.data);
+	SendMessage(hPicDisplay, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hB);
+	DeleteObject(hB);
+
+	currFrame = frame;
+}
+void videoCaptureRelease() {
+	pauseVideoCapture = true;
+	EnableVideoControls(false);
+	if (videoCapture.isOpened()) {
+		videoCapture.release();
+	}
+}
+
 void videoCaptureThread() {
 	stopVideoCapture = false;
 	while (!stopVideoCapture)
 	{
-		cv::Mat frame;
-		if (videoCapture.read(frame)) {
+		using namespace std::chrono_literals;
+		if (pauseVideoCapture) {
+			std::this_thread::sleep_for(1ms);
+		}
+		else
+		{
+			cv::Mat frame;
+			if (videoCapture.isOpened())
+				if (videoCapture.read(frame)) {
+					replaceCurrentFrame(frame);
 
-			cv::cvtColor(frame, frame, cv::COLOR_BGR2BGRA);
-
-			HBITMAP hB = CreateBitmap(frame.cols, frame.rows, 1, 32, frame.data);
-			SendMessage(hPicDisplay, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hB);
-			DeleteObject(hB);
+					if (videoLength > 0) {
+						float currFrameNum = abs(videoCapture.get(cv::CAP_PROP_POS_FRAMES));
+						SendMessage(hProgressSlider, TBM_SETPOS, true, currFrameNum);
+						SetWindowText(hTimeText, (SecToMinAndSecString(currFrameNum / cvFPS) + " / " + videoLengthString).c_str());
+					}
+					std::this_thread::sleep_for(1s / cvFPS);
+				}
 		}
 	}
 	return;
@@ -45,12 +149,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR cmdLine, i
 	mainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINDIALOG), 0, MainProc);
 	ShowWindow(mainWindow, showCmd);
 
+	camSelectorWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_CAMSOURCE), mainWindow, CamSelectProc);
+
 	MSG msg;
 	ZeroMemory(&msg, sizeof(MSG));
-
-	//Gets the available video devices
-	DeviceEnumerator de;
-	std::map<int, Device> devices = de.getVideoDevicesMap();
 
 	//Amount of threads supported by the implementation
 	int x = std::thread::hardware_concurrency();
@@ -74,6 +176,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR cmdLine, i
 }
 
 
+void EnableVideoControls(bool enable, int frameCount) {
+	EnableWindow(hPlayBtn, enable);
+
+	SendMessage(hProgressSlider, TBM_SETRANGE, true, MAKELPARAM(0, frameCount));
+	EnableWindow(hProgressSlider, enable);
+	
+	EnableWindow(hTimeText, enable);
+
+	if (!enable) {
+		videoLength = 0;
+		videoLengthString = "";
+		SetWindowText(hTimeText, "0:00 / 0:00");
+	}
+}
+
+void PauseVideoCapture(bool pause) {
+	pauseVideoCapture = pause;
+	SetWindowText(hPlayBtn, pauseVideoCapture ? "Play" : "Pause");
+}
+
 LRESULT CALLBACK MainProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
 	
 	switch (msg)
@@ -81,10 +203,66 @@ LRESULT CALLBACK MainProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
 	case WM_INITDIALOG:
 	{
 		hPicDisplay = GetDlgItem(hDlg, IDC_PICDISPLAY);
+		hPlayBtn = GetDlgItem(hDlg, IDC_BTN_PLAY);
+		hProgressSlider = GetDlgItem(hDlg, IDC_SLIDER_PROGRESS);
+		hTimeText = GetDlgItem(hDlg, IDC_TEXT_TIMER);
 
-		videoCapture.open(0);
+		EnableVideoControls(false);
 	}
 	break;
+	case WM_COMMAND:
+	{
+		int comm = LOWORD(wParam);
+		switch (comm)
+		{
+		case IDC_BTN_PLAY:
+			PauseVideoCapture(!pauseVideoCapture);
+			break;
+		case ID_FUENTE_WEBCAM:
+			ShowWindow(camSelectorWindow, SW_SHOW);
+			break;
+		case ID_FUENTE_VIDEO:
+		{
+			char buff[MAX_PATH] = "";
+			if (GetFile(hDlg, 1, buff)) {
+				videoCaptureRelease();
+				videoCapture.open(buff);
+				cvFPS = videoCapture.get(cv::CAP_PROP_FPS);
+				int frameCount = videoCapture.get(cv::CAP_PROP_FRAME_COUNT);
+				videoLength = (float)frameCount / cvFPS;
+				videoLengthString = SecToMinAndSecString(videoLength);
+				EnableVideoControls(true, frameCount);
+				PauseVideoCapture(false);
+			}
+		}
+			break;
+		case ID_FUENTE_IMAGEN:
+		{
+			char buff[MAX_PATH] = "";
+			if (GetFile(hDlg, 0, buff)) {
+				videoCaptureRelease();
+				cv::Mat frame = cv::imread(buff);
+				replaceCurrentFrame(frame);
+			}
+		}
+			break;
+		default:
+			break;
+		}
+	}
+		break;
+	case WM_HSCROLL:
+	{
+		if (LOWORD(wParam) == TB_THUMBTRACK && pauseVideoCapture) {
+			int tick = SendMessage(hProgressSlider, TBM_GETPOS, 0, 0);
+			videoCapture.set(cv::CAP_PROP_POS_FRAMES, tick);
+		}
+		else
+		{
+			PauseVideoCapture(true);
+		}
+	}
+		break;
 	case WM_DESTROY:
 	case WM_CLOSE:
 		PostQuitMessage(0);
@@ -93,5 +271,70 @@ LRESULT CALLBACK MainProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
 		break;
 	}
 	
+	return false;
+}
+
+LRESULT CALLBACK CamSelectProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg)
+	{
+	case WM_INITDIALOG:
+	{
+		hSourcesCB = GetDlgItem(hDlg, IDC_CB_SOURCES);
+		HWND hOk = GetDlgItem(hDlg, IDOK);
+
+		//Gets the available video devices
+		DeviceEnumerator de;
+		std::map<int, Device> devices = de.getVideoDevicesMap();
+
+		if (devices.size() <= 0) {
+			EnableWindow(hSourcesCB, false);
+			EnableWindow(hOk, false);
+			SetWindowText(hSourcesCB, (LPCSTR)"No se encontraron fuentes de video");
+		}
+		else
+		{
+			SetWindowText(hSourcesCB, (LPCSTR)"Seleccione una fuente de video");
+		}
+
+		for (auto const& device : devices) {
+			SendMessage(hSourcesCB, CB_ADDSTRING, device.second.id, (LPARAM)device.second.deviceName.c_str());
+		}
+	}
+		break;
+	case WM_COMMAND:
+	{
+		int comm = LOWORD(wParam);
+		switch (comm)
+		{
+		case IDOK:
+		{
+			int idx = SendMessage(hSourcesCB, CB_GETCURSEL, 0, 0);
+			if (idx > -1) {
+				videoCaptureRelease();
+				if (videoCapture.open(idx)) {
+					pauseVideoCapture = false;
+					ShowWindow(hDlg, SW_HIDE);
+				}
+				else
+					MessageBox(hDlg, "No se ha podido abrir la fuente de video seleccionada", "Error", MB_ICONEXCLAMATION);
+			}
+		}
+			break;
+		case IDCANCEL:
+			ShowWindow(hDlg, SW_HIDE);
+			break;
+		default:
+			break;
+		}
+	}
+		break;
+	case WM_DESTROY:
+	case WM_CLOSE:
+		ShowWindow(hDlg, SW_HIDE);
+		break;
+	default:
+		break;
+	}
+
 	return false;
 }
--- ProjectPI/resource.h
+++ ProjectPI/resource.h
@@ -3,15 +3,27 @@
 // Usado por ProjectPI.rc
 //
 #define IDD_MAINDIALOG                  101
+#define IDR_MENU1                       103
+#define IDD_CAMSOURCE                   104
 #define IDC_PICDISPLAY                  1001
+#define IDC_CB_SOURCES                  1002
+#define IDC_VIDEOCONTROLS               1003
+#define IDC_BTN_PLAY                    1004
+#define IDC_SLIDER_PROGRESS             1005
+#define IDC_TEXT_TIMER                  1007
+#define ID_FUENTE_WEBCAM                40001
+#define ID_FUENTE_ARCHIVODEVIDEO        40002
+#define ID_FUENTE_IM40003               40003
+#define ID_FUENTE_VIDEO                 40004
+#define ID_FUENTE_IMAGEN                40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
